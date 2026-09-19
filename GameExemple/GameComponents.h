ae0ec6c601e1b23cb407614c0345c6ad653a8983
Batap <batapxv7@gmail.com>
2026-09-19 21:19:16 +0200

    rm game

--- GameExemple/GameComponents.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Components/CelestialBody_C.h"
-#include "Components/Carryable_C.h"
-#include "Components/FpsController_C.h"
-#include "Components/Grabber_C.h"
-#include "Components/OrbitCam_C.h"
-#include "Components/Rocket_C.h"
-#include "Components/Rotator_C.h"
